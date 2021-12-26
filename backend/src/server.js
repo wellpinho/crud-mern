ac740887285f26f0803430da51511a07778193f0
wellpinho <wellpinho@outlook.com>
2021-12-26 13:01:41 -0300

    ✨ NEW FEATURE: Error routes message

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const routes = require('./modules/routes')
+const AppError = require('./errors/AppError')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
 app.use(routes)
+app.use(AppError)
 
 const port = process.env.PORT || 5000
 
